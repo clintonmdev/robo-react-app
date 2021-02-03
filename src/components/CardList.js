19a8e685f190fe03b6229df426d89766eb1924a1
Clinton <Clintonm.pro@gmail.com>
2021-02-03 02:25:51 +0100

    Robot app with searchbar and fetch features

---
+++ src/components/CardList.js
@@ -0,0 +1,22 @@
+import React  from 'react';
+
+
+const CardList = ({robots}) => {
+    const listRobots = robots.map((robot) =>
+    <div key={robot.id} className='tc bg-light-green dib br3 pa3 ma2 grow bw shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${robot.id}}?200x200`}/>
+            <div>
+                <h2>{robot.name}</h2>
+                <p>{robot.email}</p>
+            </div>
+        </div>
+    
+    );
+    return (
+        <div>
+        {listRobots}
+        </ div>
+    );
+}
+
+export default CardList
---
+++ src/containers/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leanne graww',
+        username: 'Beast',
+        email: 'Beastmode@january.biz'
+    },
+    {
+        id:2,
+        name: 'Leroy Makila',
+        username: 'Fro',
+        email: 'BayernHoliday@january.biz'
+    },
+    {
+        id:3,
+        name: 'Evil Kane',
+        username: 'Monster',
+        email: 'ManHunter@january.biz'
+    },
+    {
+        id:4,
+        name: 'Orangina',
+        username: 'Sweety wattrice',
+        email: 'SweeterthanSun@january.biz'
+    },
+    {
+        id:5,
+        name: 'Patrica Loftbroak',
+        username: 'daughter of Ragnar',
+        email: 'winteriscomming@january.biz'
+    },
+
+]
