8ca87e62f178c2f319d5a7497683f176b22a17f8
Clinton <Clintonm.pro@gmail.com>
2021-02-06 23:26:49 +0100

    Added Redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,31 @@
-import React, {useState, useEffect} from 'react'
-import CardList from '../components/CardList.js'
-import SearchBox from '../components/searchBox.js'
-import Scroll from '../components/Scroll'
-import  './App.css'
-import ErrorBoundry from '../components/ErrorBoundry'
+import React, {useState, useEffect} from 'react';
+import { connect } from 'react-redux';
+import CardList from '../components/CardList.js';
+import SearchBox from '../components/searchBox.js';
+import Scroll from '../components/Scroll';
+import  './App.css';
+import ErrorBoundry from '../components/ErrorBoundry';
 // import { robots } from './robots.js'
+import { setSearchField } from '../actions.js';
 
 
-function App(){
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
+function App(props){
     
     const [robots, setRobots] = useState([])
-    const [searchField, setSearchField] = useState('')
+    // const [searchField, setSearchField] = useState('')
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -20,19 +35,19 @@ function App(){
             setRobots(users));
         }, [])
 
-    const onSearchChange = (event) => {
-        setSearchField(event.target.value)
-    }
+    // const onSearchChange = (event) => {
+    //     setSearchField(event.target.value)
+    // }
 
     const filterRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchField.toLowerCase())})
+        return robot.name.toLowerCase().includes(props.searchField.toLowerCase())})
         
     return !robots.length ?
         <h1>Loading...</h1> :
         (
             <div className='tc'>
                 <h1 className='f2' >RoboFriends </h1>
-                <SearchBox searchChange={onSearchChange}/>
+                <SearchBox searchChange={props.onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filterRobots} />
@@ -40,10 +55,9 @@ function App(){
                 </Scroll>
             </div>
         )
-
-    }
+    };
     
 
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
